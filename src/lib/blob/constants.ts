f4d5f2748deead2370668d97c331ea02444a1dcb
blobtoshi <blobtoshi0@gmail.com>
2026-04-30 03:43:19 +0100

    Update constants.ts

--- src/lib/blob/constants.ts
+++ src/lib/blob/constants.ts
@@ -12,7 +12,7 @@ export const BASE_FEE_RATE = 10;
 export const MIN_FEE_RATE = 1;
 export const MAX_MEMO_BYTES = 80;
 
-export const GENESIS_TIME_MS = 1777515025790;
+export const GENESIS_TIME_MS = 1777517348838;
 
 // Canvas dimensions / physics for Blob Run.
 export const CW = 780;
